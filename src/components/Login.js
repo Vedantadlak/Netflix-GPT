badf4f9fb62575fee2fc4fd1ba333e970b0cfc09
VEDANT ADLAK <Vedantadlak2105@gmail.com>
2023-12-22 16:29:08 +0530

    minor css improvements

--- src/components/Login.js
+++ src/components/Login.js
@@ -95,7 +95,7 @@ const Login = () => {
       </div>
       <form
         onSubmit={(e) => e.preventDefault()}
-        className="w-full md:w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80"
+        className="w-full md:w-3/12 absolute p-12 bg-black my-12 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80"
       >
         <h1 className="font-bold text-3xl py-4">
           {isSignInForm ? "Sign In" : "Sign Up"}
@@ -121,6 +121,12 @@ const Login = () => {
           placeholder="Password"
           className="p-4 my-4 w-full bg-gray-700"
         />
+        {!isSignInForm && (
+          <p className="text-gray-400 mt-2 text-sm">
+            Password must be more than 6 character, contains one numeric,
+            special, and uppercase character.
+          </p>
+        )}
         <p className="text-red-500 font-bold text-lg py-2">{errorMessage}</p>
         <button
           className="p-4 my-6 bg-red-700 w-full rounded-lg"
